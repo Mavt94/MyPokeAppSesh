b898814df078fa986af57888a8eddb12ac4508c3
Manuel Vera <manuel.vera@alten.es>
2024-02-05 21:16:34 +0100

    - Delete icons

---
+++ MyPokeAppSesh/MyPokeAppSesh/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  MyPokeAppSesh
+//
+//  Created by Manuel Vera Trinidad on 5/2/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
